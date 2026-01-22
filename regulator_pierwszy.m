636e2b078f3d88b8a155b716a226f7c553457e0e
JozMroz <praca.jozefmroZ@GMAIL.COM>
2026-01-22 13:06:42 +0100

    Update regulator_pierwszy.m

--- regulator_pierwszy.m
+++ regulator_pierwszy.m
@@ -1,25 +1,41 @@
-%regulator wyznaczony na zajęciach
+%zadanie PID (1) - dobieranie nastaw zieglera nicholsa 1
 
-
-dane1 = readtable('SCOPE\scope_1 (1)');
+dane1 = readtable('SCOPE\scope_0 (1)');
 dane1 = dane1(4:end, :);
 
 t  = dane1{:,1};
 x1 = dane1{:,2};
 x2 = dane1{:,3};
 
-idx_skoku = find(diff(x1) > max(diff(x1))*0.5, 1, 'first');
-window = 10;
-level_start_x1 = mean(x1(idx_skoku-window : idx_skoku-1));
-level_start_x2 = mean(x2(idx_skoku-window : idx_skoku-1));
+idx_przed = t < -8.1;
+idx_po = t > -8.05;
 
-idx_ustalone = idx_skoku + round(length(t(t > -5 & t < -3))); % przykładowy zakres
-level_end_x1 = mean(x1(idx_ustalone : idx_ustalone + window));
+start_x1 = mean(x1(idx_przed));
+start_x2 = mean(x2(idx_przed));
+ofset = start_x2-start_x1;
+x1_corr = x1;
+x2_corr  = x2- ofset;
 
-du = level_end_x1 - level_start_x1;
-x1_norm = (x1 - level_start_x1) / du;
-y_norm = (x2 - level_start_x2) / du;
+start_x1 = mean(x1_corr(idx_przed));
+start_x2 = mean(x2_corr(idx_przed));
+end_x1 = mean(x1_corr(idx_po));
+end_x2 = mean(x2_corr(idx_po));
 
+du = end_x1 - start_x1;
+x1_norm = (x1_corr-start_x1)/du;
+y_norm = (x2_corr-start_x2)/du;
+
+figure;
 plot(t, x1_norm, t, y_norm);
-xlim([-6 -2]);
-title('odpowiedź skokowa układu z regulatorem dobranym podczas pomiarów');
+disp(['offset = ', num2str(ofset)]);
+title('odpowiedź skokowa do uzyskania parametrów K, To i Tp');
+
+%wyznaczenie K,To i Tp w simulinku
+K = 1;
+To = 0.08;
+Tp = 0.37;
+
+
+
+
+
